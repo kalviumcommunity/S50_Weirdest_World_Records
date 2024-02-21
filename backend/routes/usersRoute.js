9c3b18b9edcccbb9df4e308abde8272eb40de0ca
jithu2023 <jithumon.j@kalvium.community>
2024-02-21 12:40:13 +0530

    test

--- backend/routes/usersRoute.js
+++ backend/routes/usersRoute.js
@@ -10,7 +10,7 @@ router.get('/', async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 });
-
+// hello
 router.get("/:id", async (req, res) => {
   const id = req.params.id;
   try {
