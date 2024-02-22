b765f6e607838ade49d0bfa5d297e28f4a56000d
jithu2023 <jithumon.j@kalvium.community>
2024-02-22 09:04:44 +0530

    Updted

---
+++ frontend/src/components/Fetch.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function DummyUser() {
+  const [users, setUsers] = useState([]);
+  useEffect(()=>{
+    axios.get('http://localhost:3000/users')
+    .then((users)=>{
+      setUsers( users.data)
+    })
+    .catch((err)=>{
+      console.log(err)
+    })
+  }, []) 
+  return (
+    <>
+    <div className='ml-10 mt-10 mb-10'>
+      {users && users.map(user => (
+        <div key={user.User_ID}>
+          <h2>{user.User_ID}</h2>
+          <h2>{user.Username}</h2>
+          <h2>{user.Email}</h2>
+          <h2>{user.Password}</h2>
+          <h2>{user.Registration_Date}</h2>
+        </div>
+      ))}
+    </div>
+  </>
+  )
+}
+
+export default DummyUser;
